1156eedc20f400a4eb5ca5b5d24b2cb9cbd6eb76
Dan Clarke <clarke.dan91@gmail.com>
2023-10-19 13:20:49 +0100

    initial project set up, and recipe + shopping list app creation

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ingredients } from '../shared/ingredients.model';
+
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit{
+
+  ingredients: ingredients[] = [
+    new ingredients('Apples', 5),
+    new ingredients('Pears', 15),
+  ];
+
+  constructor() {  }
+
+  ngOnInit(): void {
+    
+  }
 
 }
